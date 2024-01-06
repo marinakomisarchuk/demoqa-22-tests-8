725b56ce00507b35ca99e756f0a9b5b377495a2b
marinakomisarchuk <91791839+marinakomisarchuk@users.noreply.github.com>
2024-01-06 07:26:59 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,24 +1,28 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import pages.LoginPage;
 import pages.RegistrationPage;
 
     public class TestBase {
 
-        RegistrationPage registrationPage = new RegistrationPage();
-
-        LoginPage loginPage = new LoginPage();
-
         @BeforeAll
         static void beforeAll() {
             Configuration.baseUrl = "https://demoqa.com";
             Configuration.browserSize = "1920x1080";
 //          Configuration.browser = "chrome";
-//          Configuration.timeout = 10000;
-            Configuration.holdBrowserOpen = true;
+            Configuration.timeout = 1000;
+            Configuration.holdBrowserOpen = false;
         }
+
+        @AfterEach
+        void afterEach() {
+            Selenide.closeWebDriver();
+        }
+
 
     }
 
