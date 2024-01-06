063b0858dddb767d67421a7ef15f3c8eb5960e1b
marinakomisarchuk <91791839+marinakomisarchuk@users.noreply.github.com>
2024-01-06 07:28:14 +0300

    Update TextBoxTests.java

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -3,12 +3,15 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.LoginPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests extends TestBase{
+
+    LoginPage loginPage = new LoginPage();
 
     @Test
     void fillFormTest() {
